b74cbf5d466988319a0627a93c9e639f373645ca
ahpost <rickyhwung@163.com>
2019-05-10 16:51:46 +0800

    first

--- handWriting_Ocr.py
+++ handWriting_Ocr.py
@@ -10,11 +10,15 @@ def get_file_content(filePath):
     with open(filePath, 'rb') as fp:
         return fp.read()
 
-filepath = 'example.jpg'
+filepath1 = 'example.jpg'
+filepath = 'timg8.jpg'
 image = get_file_content(filepath)
 
 options = {}
 options["recognize_granularity"] = "big"
 
 res_ocr = client.handwriting(image, options)
-print(res_ocr)
+
+res_ocr_words_res = res_ocr['words_result']
+for i in range(0,res_ocr_words_res.__len__()):
+    print(res_ocr_words_res[i]['words'])
